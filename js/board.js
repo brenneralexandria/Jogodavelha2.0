54f51def4cbef82b00c77e4536d7b4c5dfb33e42
brenneralexandria <obrenneralexandria@gmail.com>
2022-11-29 20:01:22 -0300

    Adicionando a Class board

---
+++ js/board.js
@@ -0,0 +1,7 @@
+export class Board{
+    constructor(player1, player2, placesBoard){
+        this.player1 = player1;
+        this.player2 = player2;
+        this.placesBoard = placesBoard;
+    }
+}
